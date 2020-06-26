8698eb0adcb3d9260f7d936a626f276f3ee5abc1
Andrew <goluschkin.a@yandex.by>
2020-06-26 17:34:15 +0300

    added sections: work & design

--- src/public/js/load_img.js
+++ src/public/js/load_img.js
@@ -1,4 +1,6 @@
 import img from '../img/img-1.jpg'
 import img2 from '../img/img-2.jpg'
 import img3 from '../img/img-3.jpg'
+import tablet from '../img/tablet.png'
+import mobile from '../img/mobile.png'
 
