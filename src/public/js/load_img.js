f7aa44a5bf20fe99d50d674a9896f4f70353d11d
Andrew <goluschkin.a@yandex.by>
2020-06-26 22:07:17 +0300

    added section service

--- src/public/js/load_img.js
+++ src/public/js/load_img.js
@@ -3,4 +3,5 @@ import img2 from '../img/img-2.jpg'
 import img3 from '../img/img-3.jpg'
 import tablet from '../img/tablet.png'
 import mobile from '../img/mobile.png'
+import service_img from '../img/service_img.jpg'
 
---
+++ src/public/js/service_btn.js
@@ -0,0 +1,41 @@
+const arrows = document.querySelectorAll('.arrow')
+const contents = document.querySelectorAll('.content')
+let current_i = undefined
+
+function toggle(out = false){
+    if (current_i === undefined) {
+        return
+    }
+    arrows[current_i].classList.toggle('active')      
+    
+    if (out) {
+
+        contents[current_i].classList.toggle('animate')  
+        contents[current_i].classList.toggle('animate-out')  
+        setTimeout(()=>{
+            contents[current_i].classList.toggle('active')
+            current_i = undefined
+        },270)
+    }
+    else{
+        contents[current_i].classList.toggle('animate')  
+        contents[current_i].classList.toggle('active')
+    }   
+    
+}
+
+
+arrows.forEach((item, i) =>{
+    item.addEventListener('click', (e)=>{
+        if (e.target.dataset.click) {
+            toggle(true)
+            e.target.dataset.click = ''
+        }
+        else{
+            toggle()
+            current_i = i
+            e.target.dataset.click = 'true'
+            toggle()
+        } 
+    })
+})
