e8b3aad137b0631d0dd32582ef2be10989cb6e96
Andrew <goluschkin.a@yandex.by>
2020-06-28 16:10:56 +0300

    before refactor class wrapper

--- src/public/js/load_img.js
+++ src/public/js/load_img.js
@@ -1,7 +1,24 @@
+/* about section*/
 import img from '../img/img-1.jpg'
 import img2 from '../img/img-2.jpg'
 import img3 from '../img/img-3.jpg'
+
+
+/* design section */
 import tablet from '../img/tablet.png'
 import mobile from '../img/mobile.png'
 import service_img from '../img/service_img.jpg'
+
+/*team seaction*/
+import team1 from '../img/team_1.jpg'
+import team2 from '../img/team_2.jpg'
+import team3 from '../img/team_3.jpg'
+
+/* logos section*/
+import logo1 from '../img/logo-1.png'
+import logo2 from '../img/logo-2.png'
+import logo3 from '../img/logo-3.png'
+import logo4 from '../img/logo-4.png'
+import logo5 from '../img/logo-5.png'
+import logo6 from '../img/logo-6.png'
 
